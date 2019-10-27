8f651d418cd81920a5687e89157811324206fab5
Anton Goncharov <gavmail1985@yandex.ru>
2019-10-27 15:42:18 +0300

    Script for vms.

---
+++ qemu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+script_rp="$(realpath "${0}")"         || exit 1
+script_dir="$(dirname "${script_rp}")" || exit 1
+conf_rp="${script_dir}/qemu.conf.sh"
+source "$conf_rp"                      || exit 1
+
+
+action_run() {
+	qemu-system-x86_64                         \
+		-m 2G                              \
+		-hda "${virt_hdd}"                 \
+		-snapshot                          \
+		-nodefaults                        \
+		-nographic                         \
+		-vga none                          \
+		-serial stdio
+}
+
+main() {
+	if test $# -ne 1; then
+		echo "error: " >&2
+		return 1
+	fi
+	local action="$1"; shift
+	case "${action}" in
+	run) ;;
+	*)	echo "error: bad action '${action}'" >&2
+		return 1
+		;;
+	esac
+	"action_${action}" "$@"
+}
+
+main "$@"
