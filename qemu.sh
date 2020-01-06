feebd710660a6161697c9676bf6433b6ca0f810c
Anton Goncharov <gavmail1985@yandex.ru>
2020-01-06 23:45:26 +0300

    Mount qcow2.

--- qemu.sh
+++ qemu.sh
@@ -5,6 +5,75 @@ script_dir="$(dirname "${script_rp}")" || exit 1
 conf_rp="${script_dir}/qemu.conf.sh"
 source "$conf_rp"                      || exit 1
 
+action_mount() {
+	if test $# -ne 1; then
+		echo error: >&2
+		exit 1
+	fi
+	sudo "${script_rp}" mount_root "$@"
+}
+
+action_mount_root() {
+	local img="$1"
+	if lsmod | grep nbd > /dev/null 2>&1; then
+		true
+	else
+		echo invoking modprobe nbd...
+		modprobe nbd max_part=16 || return 1
+	fi
+	# block device variables
+	local bd_idx bd_tmp block_dev block_dev_bn
+	for ((bd_idx=0;;++bd_idx)); do
+		bd_tmp="/dev/nbd${bd_idx}"
+		test -b "$bd_tmp" || break
+		if ! lsblk "$bd_tmp" > /dev/null 2>&1; then
+			block_dev="$bd_tmp"
+			block_dev_bn="$(basename "$block_dev")" || return 1
+			break
+		fi
+	done
+	if test x"$block_dev" = x; then
+		echo "error: can't find free nbd device" >&2
+		return 1
+	fi
+	local img_mounted="${img}.mounted"
+	if test -f "$img_mounted"; then
+		printf "error: image %s is mounted at '%s' device.\n" "$img" "$(cat "$img_mounted")" >&2
+		return 1
+	fi
+	qemu-nbd -c "$block_dev" "$img" || return 1
+	echo image "$img" is mounted to "$block_dev"
+	echo "${block_dev_bn}" > "$img_mounted"
+}
+
+action_umount() {
+	if test $# -ne 1; then
+		echo error: >&2
+		exit 1
+	fi
+	sudo "${script_rp}" umount_root "$@"
+}
+
+action_umount_root() {
+	local img="$1" img_mounted block_dev block_dev_bn
+	img_mounted="${img}.mounted"
+	if ! test -f "$img_mounted"; then
+		printf "error: image '%s' is not mounted (mount-file is absent).\n" "${img}" >&2
+		return 1
+	fi
+	block_dev_bn="$(cat "$img_mounted")" || return 1
+	block_dev="/dev/${block_dev_bn}"
+	printf "unmounting %s\n" "$block_dev"
+	qemu-nbd -d "${block_dev}" || return 1
+	rm -vf "$img_mounted"
+	if lsmod | grep nbd > /dev/null 2>&1; then
+		echo lsmod nbd rmmod
+		rmmod nbd || return 1
+	else
+		echo lsmod nbd not loaded
+	fi
+}
+
 action_run() {
 	sudo "${script_rp}" run_root "$@"
 }
@@ -106,6 +175,8 @@ main() {
 	fi
 	local action="$1"; shift
 	case "${action}" in
+	mount) ;;
+	mount_root) ;;
 	run) ;;
 	run_root) ;;
 	run_setup) ;;
@@ -113,6 +184,8 @@ main() {
 	run_snapshot) ;;
 	run_snapshot_root) ;;
 	snapshot) ;;
+	umount) ;;
+	umount_root) ;;
 	*)	echo "error: bad action '${action}'" >&2
 		return 1
 		;;
